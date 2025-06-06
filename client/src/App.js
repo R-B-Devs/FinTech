03f7418a3fafe3916fc0400ae317d0b972fb4991
Silindile Shabangu <silindileshabangu01@gmail.com>
2025-06-06 14:40:23 +0200

    added new pages to merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
+import DemoPage from "./pages/DemoPage";
 import ForgotPassword from "./pages/ForgotPassword";
-
+ 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-      </Routes>
-    </Router>
+<Router>
+<Routes>
+<Route path="/" element={<LandingPage />} />
+<Route path="/login" element={<LoginPage />} />
+<Route path="/demo" element={<DemoPage />} />
+<Route path="/forgot-password" element={<ForgotPassword />} />
+</Routes>
+</Router>
   );
 }
-
+ 
 export default App;
