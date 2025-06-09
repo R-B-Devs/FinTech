6a122db014b23ef630819eb149a5ad599c2288ee
ManeoMalatsi <agnesmaneomalatsi@gmail.com>
2025-06-09 16:19:18 +0200

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+// App.js
+
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
+import DemoPage from "./pages/DemoPage";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import ResetPassword from "./pages/ResetPassword";
+import RegistrationPage from "./pages/Registration-page"; // ✅ Correct import
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/demo" element={<DemoPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/register" element={<RegistrationPage />} /> {/* ✅ Correct usage */}
+      </Routes>
+    </Router>
   );
 }
 
