bbb6ec15eaeb3c5b63ba98d0401153ad5a2c9152
Neo Thobela <neo.thobela0002@gmail.com>
2025-06-10 12:22:28 +0200

    Modified register page

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import LoginPage from "./pages/LoginPage";
 import DemoPage from "./pages/DemoPage";
 import ForgotPassword from "./pages/ForgotPassword.jsx";
 import ResetPassword from "./pages/ResetPassword";
-import RegistrationPage from "./pages/Registration-page"; // ✅ Correct import
+import RegistrationPage from "./pages/registration-page"; // ✅ Correct import
 import OtpPage from "./pages/OtpVerification.jsx"
 import VerifyOtp from "./pages/VerifyOtp.jsx";
 
