41316ddf41ab9d9907dcca05074fd261ff0ab72a
XolaniMba <xolanimbambo22@gmail.com>
2025-06-19 14:37:15 +0200

    Registration page conflict resolution

--- client/src/pages/Registration-page.jsx
+++ client/src/pages/Registration-page.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import registerImage from '../assets/register.png';
-import '../styles/registration-page.css'; 
+import '../styles/registration-page.css';
 
 function RegistrationForm() {
   const navigate = useNavigate();
